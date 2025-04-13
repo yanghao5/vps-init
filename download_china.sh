d434003ec81b4ca2c4a9baa273c3de2b531fca18
Young <yanghao.buffer@outlook.com>
2025-04-13 17:13:59 +0800

    [download](china) fix download url
    remove additional head

--- download_china.sh
+++ download_china.sh
@@ -2,5 +2,5 @@
 REMOTE=${REMOTE:-https://ghfast.top/}
 apt-get update && apt-get install git wget -y
 wget ${REMOTE}https://raw.githubusercontent.com/yanghao5/vpsinit/refs/heads/main/tencent_china.sh
-git clone --bare ${REMOTE}https://ghfast.top/https://github.com/ohmyzsh/ohmyzsh.git
+git clone --bare ${REMOTE}https://github.com/ohmyzsh/ohmyzsh.git
 wget ${REMOTE}https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
