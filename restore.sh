cb87c0ee05c17b1e2171971b409b33dee6a04dbc
Young <yanghao.buffer@outlook.com>
2025-04-17 17:13:09 +0800

    [restore]: update

--- restore.sh
+++ restore.sh
@@ -24,6 +24,7 @@ rsync -rv file /home/hall/
 rsync -rv dev /home/hall/
 
 # delete files
+cd ~
 rm llvm.sh nvm_install.sh ohmyzsh_install.sh restore.sh tencent_user.sh
 rm cmake-3.28.2-linux-x86_64.tar.gz
 rm go1.24.2.linux-amd64.tar.gz
