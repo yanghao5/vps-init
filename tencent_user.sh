ca61a61f6d232f713515cf0892d277a587f5cfb9
Young <yanghao.buffer@outlook.com>
2025-04-17 16:50:53 +0800

    [tencent](user): update

--- tencent_user.sh
+++ tencent_user.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rm llvm.sh nvm_install.sh ohmyzsh_install.sh restore.sh
+rm llvm.sh nvm_install.sh ohmyzsh_install.sh restore.sh tencent_user.sh
 rm cmake-3.28.2-linux-x86_64.tar.gz
 rm go1.24.2.linux-amd64.tar.gz
 rm backup.tar.gz
