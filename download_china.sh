d859372ea0999bc7ec020fe50d55218b2a3fec2b
yanghao5 <yanghao.buffer@outlook.com>
2025-04-17 14:09:15 +0800

    [remove] remove obsoleted file

--- download_china.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-REMOTE=${REMOTE:-https://ghfast.top/}
-apt-get update && apt-get install git wget -y
-wget ${REMOTE}https://raw.githubusercontent.com/yanghao5/vpsinit/refs/heads/main/tencent_china.sh
-git clone --bare ${REMOTE}https://github.com/ohmyzsh/ohmyzsh.git
-wget ${REMOTE}https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
