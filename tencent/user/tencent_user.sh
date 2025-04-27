0c687569e410edd8c5afc4afe24da40a2f05b77e
yanghao5 <yanghao.buffer@outlook.com>
2025-04-27 23:15:43 +0800

    [tencent](user): update

--- tencent/user/tencent_user.sh
+++ tencent/user/tencent_user.sh
@@ -1,22 +1,11 @@
 #!/bin/sh
 
-# install 
-bash nvm_install.sh 
+# go 1.24.2
 sudo tar -C /usr/local -xzf go1.24.2.linux-amd64.tar.gz
 
-
-## LLVM
+# LLVM 17
 sudo apt-get install lsb-release wget software-properties-common gnupg -y
-sudo ./llvm.sh 17 all
+sudo ./llvm.sh 17 all -m https://mirrors.tuna.tsinghua.edu.cn/llvm-apt
+
 ## cmake
 sudo tar -C /opt -xzf cmake-3.28.2-linux-x86_64.tar.gz
-
-## gh
-(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
-	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
-        && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
-        && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
-	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
-	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-	&& sudo apt update \
-	&& sudo apt install gh -y
