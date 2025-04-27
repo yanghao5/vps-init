91af8853771a15634cf8ab8aba296ce2e972fb00
yanghao5 <yanghao.buffer@outlook.com>
2025-04-27 23:10:47 +0800

    [tencent](download)(china): update

--- tencent/download/tencent_download_china.sh
+++ tencent/download/tencent_download_china.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 REMOTE=${REMOTE:-https://ghfast.top/}
 apt-get update && apt-get install git wget -y
-wget ${REMOTE}https://raw.githubusercontent.com/yanghao5/vpsinit/refs/heads/main/tencent_china.sh
+
+wget ${REMOTE}https://raw.githubusercontent.com/yanghao5/vps-init/refs/heads/main/tencent/root/tencent_root_china.sh
+wget ${REMOTE}https://raw.githubusercontent.com/yanghao5/vps-init/refs/heads/main/restore.sh
+wget ${REMOTE}https://raw.githubusercontent.com/yanghao5/vps-init/refs/heads/main/tencent/user/tencent_user_china.sh
+
 git clone --bare ${REMOTE}https://github.com/ohmyzsh/ohmyzsh.git
-wget ${REMOTE}https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+
+# Cmake 3.28.2
+wget ${REMOTE}https://github.com/Kitware/CMake/releases/download/v3.28.2/cmake-3.28.2-linux-x86_64.tar.gz
+
+# LLVM 
+wget https://mirrors.tuna.tsinghua.edu.cn/llvm-apt/llvm.sh
+
+# Golang 1.24.2
+wget https://go.dev/dl/go1.24.2.linux-amd64.tar.gz
