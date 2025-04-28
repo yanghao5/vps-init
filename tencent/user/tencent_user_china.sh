63a69ba6a6d73f91957454f6837aa685a16d3e64
yanghao5 <yanghao.buffer@outlook.com>
2025-04-28 21:03:02 +0800

    [tencent](user)(china): update

--- tencent/user/tencent_user_china.sh
+++ tencent/user/tencent_user_china.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-# install 
-bash nvm_install.sh 
+# nvm
+export NVM_DIR="$HOME/.nvm" && (
+  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+  cd "$NVM_DIR"
+  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+) && \. "$NVM_DIR/nvm.sh"
+
+# go 1.24.2
 sudo tar -C /usr/local -xzf go1.24.2.linux-amd64.tar.gz
 
 
