5f3a3246cdbc1b17c0b47cb9a070161638f3ab2e
yanghao5 <yanghao.buffer@outlook.com>
2025-06-09 23:19:59 +0800

    [local] update user.sh

--- local/user.sh
+++ local/user.sh
@@ -8,7 +8,7 @@ sudo tar -C /usr/local -xzf go1.24.2.linux-amd64.tar.gz
 
 # LLVM 17
 sudo apt-get install lsb-release wget software-properties-common gnupg -y
-sudo ./llvm.sh 17 all -m https://mirrors.tuna.tsinghua.edu.cn/llvm-apt
+sudo ./llvm.sh 17 all
 
 ## cmake
 sudo tar -C /opt -xzf cmake-3.28.2-linux-x86_64.tar.gz
