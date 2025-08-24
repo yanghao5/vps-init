01533b428aa4fb4548a587926ad053a27de3d7b9
yanghao5 <yanghao.buffer@outlook.com>
2025-08-25 02:29:49 +0800

    [tencent](china/user) update

--- tencent/china/user.sh
+++ tencent/china/user.sh
@@ -2,7 +2,7 @@
 
 # nvm
 export NVM_DIR="$HOME/.nvm" && (
-  git clone /home/hall/nvm.git "$NVM_DIR"
+  git clone /home/momo/nvm.git "$NVM_DIR"
   cd "$NVM_DIR"
   git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
 ) && \. "$NVM_DIR/nvm.sh"
