7c95d138db146131bedb0ef5406a8b90b342e78f
yanghao5 <yanghao.buffer@outlook.com>
2025-08-25 19:34:46 +0800

    [tencent](global/backup) update

--- tencent/global/backup.sh
+++ tencent/global/backup.sh
@@ -3,20 +3,24 @@ mkdir -p backup/config
 
 # config 
 ## zsh
-cp .zshrc /home/hall/backup/config/
-cp .zsh_envs /home/hall/backup/config/
-cp .zsh_aliases /home/hall/backup/config/
-rsync -rv --exclude 'authorized_keys' /home/hall/.ssh /home/hall/backup/config/ 
+cp .zshrc /home/momo/backup/config/
+cp .zsh_envs /home/momo/backup/config/
+cp .zsh_aliases /home/momo/backup/config/
+
+# ssh
+rsync -rv --exclude 'authorized_keys' /home/momo/.ssh /home/momo/backup/config/ 
 
 ## neovim
-rsync -rv /home/hall/.config/nvim /home/hall/backup/config/
+rsync -rv /home/momo/.config/nvim /home/momo/backup/config/
 
 ## git
-cp .gitconfig /home/hall/backup/config/
+cp .gitconfig /home/momo/backup/config/
 
 # apps file dev
-sudo rsync -rv apps /home/hall/backup/
-rsync -rv file /home/hall/backup/ 
-sudo rsync -rv dev /home/hall/backup/ 
+sudo rsync -rv apps /home/momo/backup/
+rsync -rv file /home/momo/backup/ 
+sudo rsync -rv dev /home/momo/backup/ 
 
-zip -r backup.zip backup
+timestamp=$(date "+%Y%m%d%H%M")
+filename="backup_${timestamp}.zip"
+zip -r ${filename} backup
